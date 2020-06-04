33ede2bf5a97a75397e45299f1b48823640fcc93
IrohToph <63539191+IrohToph@users.noreply.github.com>
2020-06-04 22:36:24 +0700

    Update TomorrowPrectionStockClass.java

--- Bai_tap_lon_OOP/src/ledung/TomorrowPrectionStockClass.java
+++ Bai_tap_lon_OOP/src/ledung/TomorrowPrectionStockClass.java
@@ -1,16 +1,21 @@
-package ledung;
+package Sentence;
 
 import java.util.Date;
 
 import Input.InputData;
 import Input.STOCK;
 
-public class TomorrowPrectionStockClass {
+public class TomorrowPredictionStockClass extends TomorrowPrediction{
 	
 	private String stockClass;
+	private String linkWord;
 	private Date date;
 	
-	public TomorrowPrectionStockClass(Date date, String name) {
+	public TomorrowPredictionStockClass(Date date, String name) {
+		super();
+		this.subject = " phiên giao dịch ngày mai";
+		this.verb = " sẽ tiếp tục là một ngày giao dịch chứng kiến sự";
+		this.linkWord = "Nếu diễn biến của ngày giao dịch hôm nay còn tiếp diễn thì";
 		this.stockClass = name;
 		this.date = date;
 	}
@@ -36,29 +41,30 @@ public class TomorrowPrectionStockClass {
 		return cnt;
 	}
 	
-	public String createClause() {
+	public void setComplement() {
+		
 		int cnt = this.increaseCounter();
-		String prediction = "Nếu diễn biến còn tiếp tục như ngày hôm nay thì phiên giao dịch ngày mai sẽ tiếp tục là một phiên giao dịch";
-		String status = null;
+		
 		if ( cnt >= 20 ) {
-			status = InputData.getRandom(AdjectiveStatus.getManyIncrease()) + "của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư có thể bước vào ngày giao dịch mới với tâm lý vô cùng thoải mái.";
+			this.complement = InputData.getRandom(Verb.getManyIncrease()) + " của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư có thể bước vào ngày giao dịch mới với tâm lý vô cùng thoải mái.";
 		}
 		else if (cnt >= 17 && cnt < 20) {
-			status = InputData.getRandom(AdjectiveStatus.getFewIncrease()) + "của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư nên cân nhắc kỹ với những lựa chọn của mình.";
+			this.complement = InputData.getRandom(Verb.getFewIncrease()) + " của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư nên cân nhắc kỹ với những lựa chọn của mình.";
 		}
 		else if (cnt > 13 && cnt < 17) {
-			status = InputData.getRandom(AdjectiveStatus.getLessChanging()) + "của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư cần tiếp cận thị trường với một tâm lý vô cùng thận trọng.";
+			this.complement = InputData.getRandom(Verb.getLessChanging()) + " của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư cần tiếp cận thị trường với một tâm lý vô cùng thận trọng.";
 		}
 		else if (cnt <= 13 && cnt > 10) {
-			status = InputData.getRandom(AdjectiveStatus.getFewDecrease()) + "của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư cần nghe ngóng tình hình trước khi quyết định mua vào hay bán ra cổ phiếu thuộc nhóm này.";
+			this.complement = InputData.getRandom(Verb.getFewDecrease()) + " của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư cần nghe ngóng tình hình trước khi quyết định mua vào hay bán ra cổ phiếu thuộc nhóm này.";
 		}
 		else {
-			status = InputData.getRandom(AdjectiveStatus.getManyDecrease()) + "của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư cần phải có những quyết định chính xác và kịp thời với nhóm cổ phiếu này.";
+			this.complement = InputData.getRandom(Verb.getManyDecrease()) + " của nhóm cổ phiếu " + stockClass + ", các nhà đầu tư cần phải có những quyết định chính xác và kịp thời với nhóm cổ phiếu này.";
 		}
-		return prediction + status;
+		
 	}
 	
 	public void createSentence() {
-		System.out.println(this.createClause());
+		this.setComplement();
+		System.out.println(linkWord + subject + verb + complement);
 	}
 }
