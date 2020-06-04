27f23440c473a99cec8c0cfc08132460aaf6a8cb
IrohToph <63539191+IrohToph@users.noreply.github.com>
2020-06-04 22:35:34 +0700

    Update Sentence.java

--- Bai_tap_lon_OOP/src/ledung/Sentence.java
+++ Bai_tap_lon_OOP/src/ledung/Sentence.java
@@ -1,26 +1,24 @@
-package ledung;
+package Sentence;
 
 public class Sentence {
 	
 	protected String subject;
-	protected String object;
-	
+	protected String complement;
+	protected String verb;
 	
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 
-
-
-	public void setObject(String object) {
-		this.object = object;
-	}
-
-
-	public String createClause() {
-		return null;
+	public void setComplement(String complement) {
+		this.complement = complement;
 	}
 	
+	public void setVerb(String verb) {
+		this.verb = verb;
+	}
 	
-	
+	public void printSentence() {
+		System.out.println(subject + verb + complement);
+	}
 }
