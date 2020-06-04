79df2cf6c8062ad4b79f46ceaebdeacbce37264a
IrohToph <63539191+IrohToph@users.noreply.github.com>
2020-06-04 22:37:07 +0700

    Update DayTitle.java

--- Bai_tap_lon_OOP/src/ledung/DayTitle.java
+++ Bai_tap_lon_OOP/src/ledung/DayTitle.java
@@ -1,4 +1,4 @@
-package ledung;
+package Sentence;
 
 import java.util.Date;
 
@@ -8,59 +8,81 @@ import Input.ReadFile;
 public class DayTitle extends Title{
 	
 	private Date date;
+	private String[] subject = {"VN30", "HNX30 "};
+	private String[] verb = {"", ""};
+	private String adverb;
+	private String linkWord;
 	
 	public DayTitle(Date date) {
 		super();
 		this.date = date;
+		this.adverb = "Thị trường chứng khoán ngày " + formats.format(date) + ": ";
 	}
 	
-	public String createClause(String name) {
-		ReadFile.loadData();
+	public void setVerb() {
 		
-		String clause = name;
-		OverAllDayStockClass p = new OverAllDayStockClass(date, name);
-		int cnt = 0;
+		OverAllDayStockClass VN30data= new OverAllDayStockClass(date, "VN30");
+		OverAllDayStockClass HNX30data= new OverAllDayStockClass(date, "HNX30");
+		int VN30counter = VN30data.increaseCounter("VN30");
+		int HNX30counter = HNX30data.increaseCounter("HNX30");
+
+		if (VN30counter >= 22) {
+			this.verb[0] = InputData.getRandom(Verb.getManyIncrease());
+		}
+		else if(VN30counter < 8) {
+			this.verb[0] = InputData.getRandom(Verb.getManyDecrease());
+		}
+		else if(VN30counter >= 18 && VN30counter < 22) {
+			this.verb[0] = InputData.getRandom(Verb.getFewIncrease());
+		}
+		else if(VN30counter < 12 && VN30counter >= 8) {
+			this.verb[0] = InputData.getRandom(Verb.getFewDecrease());
+		} 
+		else {
+			this.verb[0] = InputData.getRandom(Verb.getLessChanging());
+		}
 		
-		if (name == "VN30") {
-			cnt = p.increaseCounter(name);
-			if (cnt >= 22) {
-				clause += InputData.getRandom(AdjectiveStatus.getManyIncrease());
-			}
-			else if(cnt < 8) {
-				clause += InputData.getRandom(AdjectiveStatus.getManyDecrease());
-			}
-			else if(cnt > 18 && cnt < 22) {
-				clause += InputData.getRandom(AdjectiveStatus.getFewIncrease());
-			}
-			else if(cnt < 12 && cnt > 8) {
-				clause += InputData.getRandom(AdjectiveStatus.getFewDecrease());
-			} 
-			else {
-				clause += InputData.getRandom(AdjectiveStatus.getLessChanging());
-			}
+		if (HNX30counter >= 22) {
+			this.verb[1] = InputData.getRandom(Verb.getManyIncrease());
 		}
-		else if (name == "HNX30") {
-			cnt = p.increaseCounter("HNX30");
-			if (cnt >= 22) {
-				clause += InputData.getRandom(AdjectiveStatus.getManyIncrease());
-			}
-			else if(cnt < 8) {
-				clause += InputData.getRandom(AdjectiveStatus.getManyDecrease());
-			}
-			else if(cnt > 18 && cnt < 22) {
-				clause += InputData.getRandom(AdjectiveStatus.getFewIncrease());
-			}
-			else if(cnt < 12 && cnt > 8) {
-				clause += InputData.getRandom(AdjectiveStatus.getFewDecrease());
-			} 
-			else {
-				clause += InputData.getRandom(AdjectiveStatus.getLessChanging());
-			}
+		else if(HNX30counter < 8) {
+			this.verb[1] = InputData.getRandom(Verb.getManyDecrease());
 		}
-		return clause;
+		else if(HNX30counter >= 18 && HNX30counter < 22) {
+			this.verb[1] = InputData.getRandom(Verb.getFewIncrease());
+		}
+		else if(HNX30counter < 12 && HNX30counter >= 8) {
+			this.verb[1] = InputData.getRandom(Verb.getFewDecrease());
+		} 
+		else {
+			this.verb[1] = InputData.getRandom(Verb.getLessChanging());
+		}
 	}
 	
-	public void createSentence() {
-		System.out.println("Thị trường chứng khoán ngày " + formats.format(date) + ": " + this.createClause("VN30") + ", " + this.createClause("HNX30")); 
+	public void setLinkWord() {
+		
+		OverAllDayStockClass dataVN30 = new OverAllDayStockClass(date, "VN30");
+		OverAllDayStockClass dataHNX30 = new OverAllDayStockClass(date, "VN30");
+		int counterVN30 = dataVN30.increaseCounter(dataVN30.getStockClass());
+		int counterHNX30 = dataHNX30.increaseCounter(dataHNX30.getStockClass());
+		
+		if( counterVN30 >= 18 && counterHNX30 >= 18 ||
+			counterVN30 <= 12 && counterHNX30 <= 12 ||
+			counterVN30 >= 8 && counterVN30 < 12 && counterHNX30 >= 8 && counterHNX30 < 12 ) {
+			this.linkWord = InputData.getRandom(LinkWord.getProgressiveLinkWord());
+		}
+		if( counterVN30 >= 18 && counterHNX30 < 12 ||
+			counterVN30 < 12 && counterHNX30 >= 18 ||
+			(counterVN30 < 18 || counterVN30 >= 12) && (counterHNX30 >= 18 || counterHNX30 < 12) ||
+			(counterHNX30 < 18 || counterHNX30 >= 12) && (counterVN30 >= 18 || counterVN30 < 12)) {
+			this.linkWord = InputData.getRandom(LinkWord.getOpposingLinkWord());
+		}
+	}
+	
+	@Override
+	public void printSentence() {
+		this.setLinkWord();
+		this.setVerb();
+		System.out.println(adverb + subject[0] + verb[0] + ", " + subject[1] + linkWord + verb[1]);
 	}
 }
