bafa3c0daf07f0a4a067324740a23b16eb050f4d
Trần Minh Quang <63940687+tranminhquang22012000@users.noreply.github.com>
2020-06-05 07:58:41 +0700

    Update DayChangingSentence.java

--- Bai_tap_lon_OOP/src/quang/DayChangingSentence.java
+++ Bai_tap_lon_OOP/src/quang/DayChangingSentence.java
@@ -77,17 +77,5 @@ public class DayChangingSentence {
 			}
 			else System.out.println(status.stockPricePercentageStatus());	
 	}
-	// Nhận định về thị trường, cổ phiếu
-	public void comment1() {
-		System.out.println(status.MarketOrStock() + status.stock+ " giao dịch"+ status.statusMarket() + status.timePeriod + " ngày trước. ");
-	// Tâm lí nhà đầu tư	
-	}
-	public void comment2() {
-		System.out.println("Các nhà đầu tư" + status.statusInvestor());
-	}
-	// Dự báo
-	public void comment3() {
-		System.out.println("Trong những ngày tới, " + status.stock + " sẽ" + status.forecast());
-	}
-	
+
 }
