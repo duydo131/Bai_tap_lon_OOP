bc1c21241235759bc2f07b854a2cdff118c5a7f2
Trần Minh Quang <63940687+tranminhquang22012000@users.noreply.github.com>
2020-06-07 17:29:06 +0700

    Update comment.java

--- Bai_tap_lon_OOP/src/quang/comment.java
+++ Bai_tap_lon_OOP/src/quang/comment.java
@@ -1,23 +1,27 @@
 package quang;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import Input.STOCK;
+
 public class comment {
 	public Status status;
-	// Nhận định về thị trường, cổ phiếu
-	public void comment1() {
-		System.out.println(status.MarketOrStock() + status.stock+ " giao dịch"+ status.statusMarket() + status.timePeriod + " ngày trước. ");
-		// Tâm lí nhà đầu tư	
+	public String type;
+	public String typeStatus;
+
+	public ArrayList<String> listTag;
+	public comment(Date date, STOCK stock) {
+		status= new Status(date,stock);
+		listTag = new ArrayList<String>();
 	}
-	public void comment2() {
-		System.out.println("Các nhà đầu tư" + status.statusInvestor());
-	}
-	// Dự báo
-	public void comment3() {
-		System.out.println("Trong những ngày tới, " + status.stock + " sẽ" + status.forecast());
-	}
-		
 	
-	public void createComment() {
-		System.out.println(status.comment());
+	public ArrayList<String> getTag(){
+		setTag();
+		return listTag;
 	}
 
+	public void setTag() {
+		
+	}
 }
