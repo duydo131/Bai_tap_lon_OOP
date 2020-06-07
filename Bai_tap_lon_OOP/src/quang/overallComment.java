a972c2e739acbc1c44107dd74a13228c4b346d00
Trần Minh Quang <63940687+tranminhquang22012000@users.noreply.github.com>
2020-06-07 17:49:04 +0700

    Update overallComment.java

--- Bai_tap_lon_OOP/src/quang/overallComment.java
+++ Bai_tap_lon_OOP/src/quang/overallComment.java
@@ -15,6 +15,16 @@ public class overallComment extends comment{
 		listTag.add("nhà đầu tư");
 			
 	}
+	
+	public String get() {
+		if(status.weekendCheck()) {
+			return "Các nhà đầu tư đang chờ tuần giao dịch mới.";
+		}else if(status.stock!=STOCK.VNINDEX&&status.stock!=STOCK.HASTC) {  
+			return status.comment();
+		}
+		else return "Hãy thay đổi về mã cổ phiếu cụ thể.";
+	}
+	
 	public void comment() {
 		if(status.weekendCheck()) {
 			System.out.println("Các nhà đầu tư đang chờ tuần giao dịch mới.");
