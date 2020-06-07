19eff49d250957f56b54faf5702273a6051f3399
Trần Minh Quang <63940687+tranminhquang22012000@users.noreply.github.com>
2020-06-07 17:47:22 +0700

    Update commentMarket.java

--- Bai_tap_lon_OOP/src/quang/commentMarket.java
+++ Bai_tap_lon_OOP/src/quang/commentMarket.java
@@ -15,15 +15,25 @@ public class commentMarket extends comment {
 		listTag.add("Thị trường");
 		listTag.add("đồng loạt");	
 	}
+	
+	public String get() {
+		if(status.weekendCheck()) {
+			return "Các nhà đầu tư đang chờ tuần giao dịch mới.";
+		}else if(status.stock==STOCK.VNINDEX || status.stock==STOCK.HASTC) {  
+			return "Thị trường " + status.stock+ " giao dịch"+ status.statusMarket() + "ngày trước."
+					+ " Các mã cổ phiếu cũng đồng loạt biến động theo thị trường. ";
+		}
+		else return "Hãy thay đổi về mã thị trường. ";
+	}
+	
 	public void comment() {
 		if(status.weekendCheck()) {
 			System.out.println("Các nhà đầu tư đang chờ tuần giao dịch mới.");
 		}else if(status.stock==STOCK.VNINDEX || status.stock==STOCK.HASTC) {  
-			System.out.println("Thị trường " + status.stock+ " giao dịch"+ status.statusMarket() + "ngày trước. ");
-			System.out.println("Các mã cổ phiếu cũng đồng loạt biến động theo thị trường.");
+			System.out.println("Thị trường " + status.stock+ " giao dịch"+ status.statusMarket() + "ngày trước."
+					+ " Các mã cổ phiếu cũng đồng loạt biến động theo thị trường.");
 		}
 		else System.out.println("Hãy thay đổi về mã thị trường. ");
 	}
-
 
 }
