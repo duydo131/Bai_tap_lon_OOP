daa04ba6d51f7bea08251c202e17d2267a9da125
Trần Minh Quang <63940687+tranminhquang22012000@users.noreply.github.com>
2020-06-07 17:42:26 +0700

    Update Liquidity.java

--- Bai_tap_lon_OOP/src/quang/Liquidity.java
+++ Bai_tap_lon_OOP/src/quang/Liquidity.java
@@ -13,6 +13,16 @@ public class Liquidity extends comment {
 		listTag.add("thanh khoản");
 		
 	}
+	
+	public String get() {
+		if(status.weekendCheck()) {
+			return "Các nhà đầu tư đang chờ tuần giao dịch mới.";
+		}else if(status.stock!=STOCK.VNINDEX&&status.stock!=STOCK.HASTC) {
+			return "Tính thanh khoản của cổ phiếu " + status.stock + status.Liquidity() + "trong ngày qua.";
+		}else return "Hãy thay đổi về mã cổ phiếu cụ thể."; 
+		
+	}
+	
 	public void comment() {
 		if(status.weekendCheck()) {
 			System.out.println("Các nhà đầu tư đang chờ tuần giao dịch mới.");
