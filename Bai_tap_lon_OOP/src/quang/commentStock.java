699d1eca85051efab0d5a3e694f25e147a9388c2
Trần Minh Quang <63940687+tranminhquang22012000@users.noreply.github.com>
2020-06-07 17:52:12 +0700

    Update commentStock.java

--- Bai_tap_lon_OOP/src/quang/commentStock.java
+++ Bai_tap_lon_OOP/src/quang/commentStock.java
@@ -14,6 +14,16 @@ public class commentStock extends comment {
 		listTag.add("Cổ phiếu");
 			
 	}
+	
+	public String get() {
+		if(status.weekendCheck()) {
+			return "Các nhà đầu tư đang chờ tuần giao dịch mới.";
+		}else if(status.stock!=STOCK.VNINDEX&&status.stock!=STOCK.HASTC) {  
+			return "Cổ phiếu " + status.stock+ " giao dịch"+ status.statusMarket() + "ngày trước. ";
+		}
+		else return "Hãy thay đổi về mã cổ phiếu cụ thể.";
+	}
+	
 	public void comment() {
 		if(status.weekendCheck()) {
 			System.out.println("Các nhà đầu tư đang chờ tuần giao dịch mới.");
